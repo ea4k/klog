3b897184b523de7f853613bbe51b81484be3e75a
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2009-09-22 16:55:25 +0000

    QT4 migration starts!

---
+++ trunk/src/entity.cpp
@@ -0,0 +1,149 @@
+/***************************************************************************
+                          entity.cpp  -  description
+                             -------------------
+    begin                : mar dic 17 2002
+    copyright            : (C) 2002 by Jaime Robles
+    email                : jaime@kde.org
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or modify     *
+*  it under the terms of the GNU General Public License as published by     *
+*  the Free Software Foundation; either version 2 of the License, or        *
+*  (at your option) any later version.                                      *
+ *                                                                            *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+*  GNU General Public License for more details.                             *
+ *                                                                            *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program; if not, write to the Free Software              *
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA*
+ *****************************************************************************/
+
+//#include <klocale.h>
+//#include <kdebug.h>
+
+#include "entity.h"
+
+Entity::Entity(){
+}
+
+Entity::Entity (const int tnumb, const QString& tentity, const QString& tpfx, const int tcqz, const long tlon, const long tlat, const QString& tcontinent, const int tituz,  const int ttimeZone){
+
+  numb = tnumb;
+  entity = tentity;
+  pfx = tpfx;
+  cqz = tcqz;
+  lon = tlon;
+  lat = tlat;
+  timeZone = ttimeZone;
+  continent = tcontinent;
+  ituz = tituz;
+	adifCode = 0;
+}
+/*
+Entity::Entity (const int tnumb, const QString& tentity, const QString& tpfx, const int tcqz, const long tlon, const long tlat, const QString& tcontinent, const int tituz,  const int ttimeZone){
+
+  numb = tnumb;
+  entity = tentity;
+  pfx = tpfx;
+  cqz = tcqz;
+  lon = tlon;
+  lat = tlat;
+  timeZone = ttimeZone;
+  continent = tcontinent;
+  ituz = tituz;
+}
+*/
+Entity::~Entity(){
+}
+
+void Entity::setEntity(const QString &tentity){
+  entity = tentity;
+}
+
+QString Entity::getEntity() const{
+    return entity;  // We can have the Entity name translated!
+}
+
+void Entity::setPfx(const QString &tpfx){
+    pfx = tpfx;
+}
+
+QString Entity::getPfx() const{
+    return pfx;
+}
+
+void Entity::setNumb(int tnumb){
+    numb = tnumb;
+}
+
+int Entity::getNumb() const{
+    return numb;
+}
+
+void Entity::setCqz (int tcqz){
+    cqz = tcqz;
+}
+
+int Entity::getCqz() const{
+    return cqz;
+}
+
+void Entity::setLon(double tlon){
+    lon = tlon;
+}
+
+double  Entity::getLon() const{
+    return lon;
+}
+
+void Entity::setLat(double tlat){
+    lat = tlat;
+}
+
+double Entity::getLat() const{
+    return  lat;
+}
+
+void Entity::setItuz(int tituz){
+    ituz = tituz;
+}
+
+int Entity::getItuz() const{
+    return ituz;
+}
+
+void Entity::setContinent(const QString &tcontinent){
+    continent = tcontinent;
+}
+
+QString Entity::getContinent() const{
+    return continent;
+}
+
+void Entity::addPrefix(const QString tprx){
+  prefixes.append(tprx);
+}
+
+void Entity::setTimeZone(int ttimeZone){
+    timeZone = ttimeZone;
+}
+
+int Entity::getTimeZone() const{
+    return timeZone;
+}
+
+
+
+void Entity::setAdifCode(int tAdif){
+	adifCode = tAdif;
+}
+
+
+int Entity::getAdifCode() const{
+	return adifCode;
+}
---
+++ trunk/src/workedcalls.cpp
@@ -0,0 +1,82 @@
+/***************************************************************************
+                          workedcalls  -  description
+                             -------------------
+    begin                : jue ago 19 2004
+    copyright            : (C) 2004 by Jaime Robles
+    email                : jaime@kde.org
+ ***************************************************************************/
+
+/******************************************************************************
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or modify     *
+*  it under the terms of the GNU General Public License as published by     *
+*  the Free Software Foundation; either version 2 of the License, or        *
+*  (at your option) any later version.                                      *
+ *                                                                            *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+*  GNU General Public License for more details.                             *
+ *                                                                            *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program; if not, write to the Free Software              *
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA*
+ *****************************************************************************/
+//#include <klocale.h>
+
+//#include "iostream"
+#include "workedcalls.h"
+
+//using namespace std;
+
+
+WorkedCalls::WorkedCalls(){
+//	itQR = qrzMap.begin();
+}
+WorkedCalls::~WorkedCalls(){
+}
+
+
+int WorkedCalls::addCall(const QString& workedCall, const int tnum){
+//cout << "WorkedCalls::addCall: " << workedCall << "/" << QString::number(tnum) << endl;
+	i = 0;
+	i = findCall(workedCall);
+	if (i <= 0){
+		i = tnum;
+		WorkedCalls::qrzMap[workedCall] = i;
+		return i;
+	}else{
+		//cout << "WorkedCalls::addCall: Call already worked " << endl;
+		return 0;
+	}
+}
+
+int WorkedCalls::findCall(const QString& workedCall){
+//cout << "WorkedCalls::findCall: " << workedCall << endl;
+	if ((workedCall=="NULL")||(workedCall.isEmpty())||(workedCall.isNull())){ // We are calling this function just after start KLog	
+//		cout << "WorkedCalls::findCall: NULL CALL or empty"  << endl;
+		return 0;
+	}
+
+
+// 	itQR = qrzMap.begin();
+// 	itQR = qrzMap.find(workedCall);
+// 	if (qrzMap.find(workedCall) != qrzMap.end()){
+// 		return itQR.data();
+// 	}
+// 	else
+// 		return 0;
+
+
+
+	for (itQR = qrzMap.begin(); itQR != qrzMap.end(); ++itQR){
+		//cout << "WorkedCall: " << workedCall << "/" << itQR.key() << endl;
+		if (itQR.key() == workedCall){
+//			cout << "WorkedCall: FOUND!" << workedCall << "/" << QString::number(itQR.data())  << endl;	
+                        return itQR.value();
+		}
+	
+	}
+//	cout << "WorkedCall::findCall: NOT FOUND: " << workedCall  << endl;
+	return 0;
+}
