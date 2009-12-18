1ed7850e2189b3cc8b3dad1eafb27d203c8e1f2e
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2009-12-18 16:50:28 +0000

    Added dxspot.cpp

--- trunk/src/dxspot.h
+++ trunk/src/dxspot.h
@@ -28,13 +28,14 @@
 #define DxSpot_H
 #include <QString>
 
-#include <QString>
+#include <QDateTime>
+#include <QDate>
 
 class DxSpot{
 
 public:
-   DxSpot() {};
-   ~DxSpot() {};
+   DxSpot();
+   ~DxSpot();
    DxSpot(QString sCall,  QString lCall, QString sCountry, QString lCountry,
            qreal spotLat, qreal spotLong, qreal logLat, qreal logLong, qreal freq) :
            spotCallsign(sCall), loggingCallsign(lCall), spotCountry(sCountry), loggingCountry(lCountry),
@@ -44,22 +45,46 @@ public:
    QString LogCall() const { return loggingCallsign; }
    QString SpotCountry() const { return spotCountry; }
    QString LogCountry() const { return loggingCountry; }
+   QString getDxCall() const{return dxcall;}
    qreal SLat() const { return spotLatitude; }
    qreal SLong() const { return spotLongitude; }
    qreal LLat() const { return loggingLatitude; }
    qreal LLong() const { return loggingLongitude; }
    qreal Frequency() const { return frequency; }
+   void setDxCall(const QString &tdxcall);
+   void setSpotter(const QString &tdxcall);
+   QString getSpotter();
+  void setNumb (int tnumb);
+  void setDateTime(QDateTime tdateTime);
+  int getNumb();
+  QDateTime getDateTime() const{return dateTime;}
 
+  void setDate(QDate tdate);
+  QDate getDate() const{return dateTime.date();}
+
+  void setTime(QTime ttime);
+  QTime getTime() const{return dateTime.time();}
+  void setFreq(double tfreq);
+  double getFreq() const{return freq;}
+  void clearDXSpot();
+  void setcomment(const QString &tcomment);  
+  QString getcomment() const{return comment;}
+
+  
 private:
    QString spotCallsign;
    QString loggingCallsign;
    QString spotCountry;
    QString loggingCountry;
+   QString dxcall, dxspotter, comment;
+   int numb;
+   double freq;
    qreal spotLatitude;
    qreal spotLongitude;
    qreal loggingLatitude;
    qreal loggingLongitude;
    qreal frequency;
+   QDateTime dateTime;
 
 };
 #endif
