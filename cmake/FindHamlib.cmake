9f7d5d8ed6ade922f5109896dc15da874dcbbfe8
jaime <29511627+ea4k@users.noreply.github.com>
2010-06-09 05:49:14 +0000

    AB4BD's update for cmake. Fix to make some architectures to compile in Debian

---
+++ cmake/FindHamlib.cmake
@@ -0,0 +1,47 @@
+# - Try to find Hamlib
+# Author: George L. Emigh - AB4BD
+#
+# HAMLIB_FOUND - system has Hamlib
+# HAMLIB_LIBRARY - location of the library for hamlib
+# HAMLIB_INCLUDE_DIR - location of the include files for hamlib
+
+set(HAMLIB_FOUND FALSE)
+
+find_path(HAMLIB_INCLUDE_DIR
+	NAMES rig.h
+	PATHS
+		/usr/include/hamlib
+		/usr/include
+		/usr/local/include/hamlib
+		/usr/local/include
+)
+
+find_library(HAMLIB_LIBRARY
+	NAMES hamlib
+	PATHS
+		/usr/lib64/hamlib
+		/usr/lib/hamlib
+		/usr/lib64
+		/usr/lib
+		/usr/local/lib64/hamlib
+		/usr/local/lib/hamlib
+		/usr/local/lib64
+		/usr/local/lib
+)
+
+if(HAMLIB_INCLUDE_DIR AND HAMLIB_LIBRARY)
+	set(HAMLIB_FOUND TRUE)
+#	message(STATUS "Hamlib version: ${VERSION}")
+	message(STATUS "Found hamlib library directory at: ${HAMLIB_LIBRARY}")
+	message(STATUS "Found hamlib include directory at: ${HAMLIB_INCLUDE_DIR}")
+endif(HAMLIB_INCLUDE_DIR AND HAMLIB_LIBRARY)
+
+IF(NOT HAMLIB_FOUND)
+  IF(NOT HAMLIB_FIND_QUIETLY)
+    MESSAGE(STATUS "HAMLIB was not found.")
+  ELSE(NOT HAMLIB_FIND_QUIETLY)
+    IF(HAMLIB_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "HAMLIB was not found.")
+    ENDIF(HAMLIB_FIND_REQUIRED)
+  ENDIF(NOT HAMLIB_FIND_QUIETLY)
+ENDIF(NOT HAMLIB_FOUND)
