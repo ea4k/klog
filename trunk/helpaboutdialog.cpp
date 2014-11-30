81156bed02e08ad21901d6a056ff4a2069435a28
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2014-11-30 12:59:17 +0000

    Trunk fixed

---
+++ trunk/helpaboutdialog.cpp
@@ -0,0 +1,78 @@
+#include "helpaboutdialog.h"
+//#include <QDebug>
+
+
+
+HelpAboutDialog::HelpAboutDialog(const QString tversion)
+{
+    //qDebug() << "HelpAboutDialog::HelpAboutDialog"  << endl;
+
+    QString text;
+    QString _version = tversion;
+
+    //text = "<center><h2>KLog " + _version + "</h2><h4>By EA4TV - 2002-2014</h4></center><br>KLog is a free logging software for hamradio operators.<br><br><b>Please know that this is an BETA release and it may contain many bugs.<br>Don't use this software with your real data.</b><br><br>KLog is an evolution of <a href=http://jaime.robles.es/klog>KLog</a> with the objective to provide a cross-platform application that runs in the main operating systems (Linux, OSX & Windows) and provides new functionalities that KLog was not providing. <br><br>Find more information at<br><a href=http://jaime.robles.es/klog>http://jaime.robles.es/klog</a><br><br>Author: Jaime Robles, EA4TV<br><a href=mailto:jaime@robles.es>jaime@robles.es</a>";
+    text = "<center><h2>KLog " + _version + "</h2><h4>By EA4TV - 2002-2014</h4></center><br>KLog is a free logging software for hamradio operators.<br><br><b>Please know that this is an BETA release and it may contain many bugs.<br>Backup your data before using this software!</b><br><br>KLog has been fully rewritten from the 0.6.2 to be able to provide a cross-platform application that runs in the main operating systems (Linux, OSX & Windows) and provide new functionalities that KLog was not providing. <br><br>Find more information and latest release at<br><a href=http://jaime.robles.es/klog>http://jaime.robles.es/klog</a><br><br>Author: Jaime Robles, EA4TV<br><a href=mailto:jaime@robles.es>jaime@robles.es</a>";
+    //text = "<center><h2>KLog " + _version + tr("</h2><h4>By EA4TV - 2002-2014</h4></center><br>KLog is a free logging software for hamradio operators.<br><br><b>Please know that this is an BETA release and it may contain many bugs.<br>Backup your data before using this software!</b><br><br>KLog has been fully rewritten from the 0.6.2 to be able to provide a cross-platform application that runs in the main operating systems (Linux, OSX & Windows) and provide new functionalities that KLog was not providing. <br><br>Find more information and latest release at<br><a href=http://jaime.robles.es/klog>http://jaime.robles.es/klog</a><br><br>Author: Jaime Robles, EA4TV<br><a href=mailto:jaime@robles.es>jaime@robles.es</a>");
+
+    textBrowser = new QTextBrowser;
+    textBrowser->setOpenLinks(true);
+    textBrowser->setOpenExternalLinks(true);
+    textBrowser->setHtml(text);
+
+    QPushButton *acceptButton = new QPushButton(tr("Ok"));
+
+
+    textBrowser->setOpenExternalLinks(true);
+    //textBrowser->setHTML(url);
+
+
+    QHBoxLayout *buttonsLayout = new QHBoxLayout;
+    buttonsLayout->addWidget(acceptButton);
+
+
+    ///
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(textBrowser);
+    mainLayout->addLayout(buttonsLayout);
+
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("About KLog"));
+
+    ///
+
+    connect(acceptButton, SIGNAL(clicked()), this, SLOT(slotAcceptButtonClicked()));
+}
+
+HelpAboutDialog::~HelpAboutDialog()
+{
+    //qDebug() << "HelpAboutDialog::~HelpAboutDialog"  << endl;
+}
+
+void HelpAboutDialog::slotAcceptButtonClicked()
+{
+    //qDebug() << "HelpAboutDialog::slotAcceptButtonClicked"  << endl;
+    accept();
+}
+
+
+
+void HelpAboutDialog::keyPressEvent(QKeyEvent *event){
+
+    switch (event->key()) {
+    //case Qt::Key_Return:
+        // ENTER PRESSED
+        //slotQRZReturnPressed();
+      //  break;
+    //case Qt::Key_Enter:
+
+        // ENTER PRESSED
+       // slotQRZReturnPressed();
+        break;
+
+    default:
+        //QFrame::keyPressEvent(event)
+        slotAcceptButtonClicked();
+
+    }
+}
