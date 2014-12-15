e01b3877e4c84ea6092c5994effbad542669edb8
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2014-12-15 19:46:02 +0000

    First RC of the 0.9.0.3. Bug of Data types fixed

---
+++ branches/0.9.0.3/helpaboutdialog.h
@@ -0,0 +1,29 @@
+#ifndef HELPABOUTDIALOG_H
+#define HELPABOUTDIALOG_H
+//#include <QtWidgets>
+#include <QtGui>
+#include <QDialog>
+ #include <QTextBrowser>
+
+
+class HelpAboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    HelpAboutDialog(const QString tversion);
+    ~HelpAboutDialog();
+
+private slots:
+    void slotAcceptButtonClicked();
+    //void slotCancelButtonClicked();
+
+
+
+private:
+    void keyPressEvent(QKeyEvent *event);
+
+     QTextBrowser *textBrowser;
+};
+
+#endif // HELPABOUTDIALOG_H
