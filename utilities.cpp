72d0b2b72178f54c6c80fbc68865aebe521ea148
jaime <29511627+ea4k@users.noreply.github.com>
2015-07-19 18:26:46 +0000

    Created the utilities class

---
+++ utilities.cpp
@@ -0,0 +1,23 @@
+#include "utilities.h"
+
+Utilities::Utilities()
+{
+}
+
+int Utilities::getProgresStepForDialog(int totalSteps){
+  //qDebug() << "Utilities::getProgresStepForDialog";
+    if (totalSteps <=100)
+        return 1;
+    else if (totalSteps <=1000)
+        return 5;
+    else if (totalSteps <=4000)
+        return 10;
+    else if (totalSteps <=5000)
+        return 15;
+    else if (totalSteps <=7000)
+        return 20;
+    else if (totalSteps <=9999)
+        return 25;
+    else
+        return 50;
+}
