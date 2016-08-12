fa4a79f235efb1e7f9ab6ddcf639c723ac7bcb6f
jaime <29511627+ea4k@users.noreply.github.com>
2016-08-12 15:36:49 +0000

    missing file

--- mainwindowsattab.cpp
+++ mainwindowsattab.cpp
@@ -301,7 +301,7 @@ void MainWindowSatTab::setSatelliteCombo(const QString _p)
             QMessageBox msgBox;
             msgBox.setIcon(QMessageBox::Warning);
             aux = tr("A satellite name has been detected but is not on the KLog satellite's names table. Please check that the correct satellite is selected and if the satellite is not in the list, please contact the development team to include it.\n\n");
-            msgBox.setText(aux + tr("The satellite you have in your QSO is: ") + _p + tr("\n\nPlease know that the satellite name will not be saved if it is not in the list so that information may be lost!"));
+            msgBox.setText(aux + tr("The satellite you have in your QSO is: ") + _p + "\n\nPlease know that the satellite name will not be saved if it is not in the list so that information may be lost!");
             msgBox.setStandardButtons(QMessageBox::Ok);
             msgBox.setDefaultButton(QMessageBox::Ok);
             int ret = msgBox.exec();
