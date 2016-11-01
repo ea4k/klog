765e8ad5d3b89b3bc7107db5cdae82c584e3ea64
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2016-11-01 17:41:46 +0000

    deployOSX.sh update

--- util/deployOSX.sh
+++ util/deployOSX.sh
@@ -11,12 +11,18 @@ if [ $# -ne 1 ]; then
  fi
 KLOG_VERSION="$1"
 KLOG_SOURCES="../klog"
-QTDIRi="../../Qt5.5.1/5.5/clang_64"
+QTDIRi=$HOME"/Qt5.5.1/5.5/clang_64"
+rm -R KLog.app
+/usr/bin/make clean
+$QTDIRi/bin/qmake KLog.pro
+/usr/bin/make
 mv klog.app KLog.app
 mkdir -p KLog.app/Contents/PlugIns/sqldrivers
 cp $QTDIRi/plugins/sqldrivers/libqsqlite.dylib KLog.app/Contents/PlugIns/sqldrivers
 mkdir -p KLog.app/Contents/MacOS/translations
+$QTDIRi/bin/lupdate KLog.pro
+$QTDIRi/bin/lrelease KLog.pro
 cp $KLOG_SOURCES/translations/*.qm KLog.app/Contents/MacOS/translations/
-macdeployqt KLog.app/ -dmg
+$QTDIRi/bin/macdeployqt KLog.app/ -dmg
 mv KLog.dmg KLog-$KLOG_VERSION.dmg
 echo "You can find the dmg file in this folder... enjoy KLog!"
