d3cb4472963642d7b7ccc5b4bf6a9c8b1ecf08a3
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2018-10-28 11:59:43 +0000

    Updated to new 5.10.1

--- util/deployOSX.sh
+++ util/deployOSX.sh
@@ -4,18 +4,19 @@
 if [ $# -ne 1 ]; then
 	echo "You must include the version of KLog you want to deploy as a single parameter"
 	echo ""
-	echo "example: deployOSX.sh 0.9.2 "
+	echo "example: deployOSX.sh 0.9.5"
 	echo ""
 	echo ""
 	exit -1
  fi
+export CXXFLAGS=-std=c++11
 KLOG_VERSION="$1"
 KLOG_SOURCES="../klog"
-QTDIRi=$HOME"/Qt5.5.1/5.5/clang_64"
+QTDIRi=$HOME"/Qt5.10.1/5.10.1/clang_64"
 rm -R KLog.app
 rm -R build 
 /usr/bin/make clean
-$QTDIRi/bin/qmake KLog.pro
+$QTDIRi/bin/qmake KLog.pro -spec macx-clang CONFIG+=x86_64 
 /usr/bin/make
 mv build/target/klog.app KLog.app
 mkdir -p KLog.app/Contents/PlugIns/sqldrivers
