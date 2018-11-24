da8a5e5ec89f9cf08394cd225734c3d471a5b00f
jaime <jaime@5834a56a-7829-0410-841d-b57922c102d6>
2018-11-24 23:02:55 +0000

    Util update

--- util/deployOSX.sh
+++ util/deployOSX.sh
@@ -4,7 +4,7 @@
 if [ $# -ne 1 ]; then
 	echo "You must include the version of KLog you want to deploy as a single parameter"
 	echo ""
-	echo "example: deployOSX.sh 0.9.2 "
+	echo "example: deployOSX.sh 0.9.5"
 	echo ""
 	echo ""
 	exit -1
@@ -12,7 +12,7 @@ if [ $# -ne 1 ]; then
 export CXXFLAGS=-std=c++11
 KLOG_VERSION="$1"
 KLOG_SOURCES="../klog"
-QTDIRi=$HOME"/Qt5.10.1/5.10.1/clang_64"
+QTDIRi=$HOME"/Qt/5.11.1/clang_64"
 rm -R KLog.app
 rm -R build 
 /usr/bin/make clean
@@ -22,8 +22,8 @@ mv build/target/klog.app KLog.app
 mkdir -p KLog.app/Contents/PlugIns/sqldrivers
 cp $QTDIRi/plugins/sqldrivers/libqsqlite.dylib KLog.app/Contents/PlugIns/sqldrivers
 mkdir -p KLog.app/Contents/MacOS/translations
-$QTDIRi/bin/lupdate KLog.pro
-$QTDIRi/bin/lrelease KLog.pro
+#$QTDIRi/bin/lupdate KLog.pro
+#$QTDIRi/bin/lrelease KLog.pro
 cp $KLOG_SOURCES/translations/*.qm KLog.app/Contents/MacOS/translations/
 $QTDIRi/bin/macdeployqt KLog.app/ -dmg
 mv KLog.dmg KLog-$KLOG_VERSION.dmg
