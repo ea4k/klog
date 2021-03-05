bd6e4bab75ba5e8e6a507efa9c6b96d7c5771b4b
ea4k <jaime@robles.es>
2021-03-05 13:25:50 +0100

    Update dataproxy_sqlite.cpp

--- dataproxy_sqlite.cpp
+++ dataproxy_sqlite.cpp
@@ -2762,7 +2762,8 @@ int DataProxy_SQLite::isWorkedB4(const QString &_qrz, const int _currentLog)
 
 }
 
-QList<int> DataProxy_SQLite::isThisQSODuplicated(const QString &_qrz, const QDateTime &_dateTime, const int _band, const int _mode)
+QList<int> isThisQSODuplicated(const QString &_callingFunc, const QString &_qrz, const QDateTime &_dateTime, const int _band, const int _mode, const int _secs)
+//QList<int> DataProxy_SQLite::isThisQSODuplicated(const QString &_qrz, const QDateTime &_dateTime, const int _band, const int _mode)
 {
         //qDebug() << "DataProxy_SQLite::isThisQSODuplicated" << endl;
     QSqlQuery query;
