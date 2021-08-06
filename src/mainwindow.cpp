dda435e1417ecd73390da256a0170bfdecdc7782
ea4k <jaime@robles.es>
2021-08-06 16:49:22 +0200

    Issue 23

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4596,7 +4596,7 @@ void MainWindow::slotSetupDialogFinished (const int _s)
         readConfigData();
         reconfigureDXMarathonUI(manageDxMarathon);
         logEvent(Q_FUNC_INFO, "Just after readConfigData", logSeverity);
-         //qDebug() << "MainWindow::slotSetupDialogFinished: logmodel to be created-2" << Qt::endl;
+        //qDebug() << "MainWindow::slotSetupDialogFinished: logmodel to be created-2" << Qt::endl;
         logEvent(Q_FUNC_INFO, "logmodel to be created-2", logSeverity);
         logWindow->createlogPanel(currentLog);
         logEvent(Q_FUNC_INFO, "logmodel has been created-2", logSeverity);
@@ -5016,8 +5016,10 @@ bool MainWindow::processConfigLine(const QString &_line){
         //qDebug << "MainWindow::processConfigLine: REALTIME: " << value.toUpper() << Qt::endl;
         mainQSOEntryWidget->setRealTime(util->trueOrFalse(value));
         //realTime = util->trueOrFalse(value);
-    }
-    else if (field =="DXCLUSTERSERVERTOUSE"){
+    }else if (field=="LOGVIEWFIELDS"){
+        //qDebug() << "MainWindow::processConfigLine: LOGVIEWFIELDS: " << value.toUpper() << Qt::endl;
+        logWindow->setColumns(value.split(",", Qt::SkipEmptyParts));
+    }else if (field =="DXCLUSTERSERVERTOUSE"){
         aux = value;  //dxfun.com:8000
         if (aux.contains(':'))
         {
@@ -6896,7 +6898,7 @@ void MainWindow::qsoToEdit (const int _qso)
 
                 nameCol = rec.indexOf("vucc_grids");
                 aux1 = (query.value(nameCol)).toString();
-                qDebug() << Q_FUNC_INFO << ": VUCC_GRIDS: " << aux1;
+                //qDebug() << Q_FUNC_INFO << ": VUCC_GRIDS: " << aux1;
                 if (util->isValidVUCCGrids (aux1))
                 {
                     othersTabWidget->setVUCCGrids (aux1);
