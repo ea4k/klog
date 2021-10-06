485f74dd1617355161f6ca5c4ae01042f7fc8011
ea4k <jaime@robles.es>
2021-10-06 18:10:38 +0200

    FLRig remote connection is working (closes #339 )

--- src/hamlibclass.cpp
+++ src/hamlibclass.cpp
@@ -450,7 +450,7 @@ bool HamLibClass::init(bool _active)
     // Code of DG1VS (Thank you!)
     if (myrig_model == RIG_MODEL_NETRIGCTL)
     {
-        qDebug() << "HamLibClass::init: RIG_PORT_NETWORK" << QT_ENDL;
+        //qDebug() << "HamLibClass::init: RIG_PORT_NETWORK" << QT_ENDL;
         // network based communication
         my_rig->state.rigport.type.rig = RIG_PORT_NETWORK;
         QString netAddPort = QString("%1:%2").arg (networkAddress).arg(networkPort);
@@ -460,15 +460,16 @@ bool HamLibClass::init(bool _active)
     }
     else if (myrig_model == RIG_MODEL_FLRIG)
     {
-        qDebug() << "HamLibClass::init: RIG_PORT_RPC" << QT_ENDL;
-        my_rig->state.rigport.type.rig = RIG_PORT_RPC;
+        //qDebug() << "HamLibClass::init: RIG_PORT_RPC" << QT_ENDL;
+        //my_rig->state.rigport.type.rig = RIG_PORT_RPC;
+        my_rig->state.rigport.type.rig = RIG_PORT_NETWORK;
         QString netAddPort = QString("%1:%2").arg (networkAddress).arg(networkPort);
-        qDebug() << "HamLibClass::init: " << netAddPort << QT_ENDL;
+        //qDebug() << "HamLibClass::init: " << netAddPort << QT_ENDL;
         qstrncpy (my_rig->state.rigport.pathname, netAddPort.toLocal8Bit().constData(), FILPATHLEN);
     }
     else
     {
-        qDebug() << "HamLibClass::init: !RIG_PORT_NETWORK" << QT_ENDL;
+        //qDebug() << "HamLibClass::init: !RIG_PORT_NETWORK" << QT_ENDL;
         //qDebug() << "HamLibClass::init: serialport2: " << serialPort.toLocal8Bit() << QT_ENDL;
         my_rig->state.rigport.type.rig = RIG_PORT_SERIAL;
         //strncpy (my_rig->state.rigport.pathname, serialPort.toLocal8Bit().constData(), FILPATHLEN);
@@ -489,26 +490,26 @@ bool HamLibClass::init(bool _active)
         // Config done
     }
 
-    qDebug() << "HamLibClass::init: after handshake "  << QT_ENDL;
+    //qDebug() << "HamLibClass::init: after handshake "  << QT_ENDL;
     // Config done
     retcode = rig_open(my_rig);
-    qDebug() << "HamLibClass::init: retcode"  << QT_ENDL;
+    //qDebug() << "HamLibClass::init: retcode"  << QT_ENDL;
 
     if (retcode != RIG_OK)
     {
-        qDebug()<< "HamLibClass::init: Can't open: " << rigerror(retcode) << QT_ENDL;
+        //qDebug()<< "HamLibClass::init: Can't open: " << rigerror(retcode) << QT_ENDL;
 
         rig_cleanup(my_rig);
         return errorManage (retcode);
     }
 
-    qDebug()<< "HamLibClass::init: Rig open!"  << QT_ENDL;
+    //qDebug()<< "HamLibClass::init: Rig open!"  << QT_ENDL;
     errorCount = 0;
     rigLaunched = true;
     freq_old = 0.0;
     timer->start(pollInterval);
 
-    qDebug() << "HamLibClass::init: END TRUE" << QT_ENDL;
+    //qDebug() << "HamLibClass::init: END TRUE" << QT_ENDL;
     return true;
 }
 
