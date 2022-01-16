be2000eac1f09a67b6488326fe239105b5ebff9e
Juan carlos <ecoalfa5wa@gmail.com>
2022-01-16 18:43:52 +0100

    Conflict with github

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3183,23 +3183,11 @@ void MainWindow::cleanQRZCOMreceivedDataFromUI()
     //qDebug() << Q_FUNC_INFO;
     if (!modify)
     {
-<<<<<<< HEAD
         QSOTabWidget->cleanQRZCOM(true);
-=======
-//        if (qrzcomSubscriber)
-//        {
-//            qrzAutoChanging = false;
-//        }
-//        else
-//        {
-//            qrzAutoChanging = true;
-//        }
-        QSOTabWidget->cleanQRZCOM(!qrzcomSubscriber);
->>>>>>> master
+    }
         completedWithPreviousName = false;
         completedWithPreviousName = false;
         completedWithPreviousLocator = false;
-    }
 }
 
 void MainWindow::slotElogQRZCOMFoundData(const QString &_t, const QString & _d)
