9a21c580b29a7370fd39ba1a991cc877f3d8636b
ea4k <jaime@robles.es>
2022-10-06 12:10:55 +0200

    Windows automation

--- devscripts/KLogWinCreatePackage2.bat
+++ devscripts/KLogWinCreatePackage2.bat
@@ -1,7 +1,7 @@
 echo off
 rem Script to deploy the windows package file
 rem Must be executed in the build directory of KLog
-rem***************************************************************************
+rem ***************************************************************************
 rem			
 rem                         klogwincreatepacksage2.bat
 rem						  -------------------
@@ -9,7 +9,7 @@ rem    begin                : jan 2021
 rem    copyright            : (C) 2021 by Jaime Robles
 rem    email                : jaime@robles.es
 rem ***************************************************************************
-rem/*****************************************************************************
+rem /*****************************************************************************
 rem * This file is part of KLog.                                                *
 rem *                                                                           *
 rem *    KLog is free software: you can redistribute it and/or modify           *
@@ -27,17 +27,25 @@ rem *    along with KLog.  If not, see <https://www.gnu.org/licenses/>.         
 rem *                                                                           *
 rem *****************************************************************************/@echo off
 echo Setting up environment for Qt usage...
-set PATH=C:/Qt/5.15.2/mingw81_32/bin;C:/Qt/Tools/mingw810_32/bin;%PATH%
+set KLOGDEVELVERSION=2.5
+
 cd ../src/
-rmdir /S /Q build
-rmdir /S /Q release
-rmdir /S /Q debug
+rmdir /s /q build
+rmdir /s /q release
+rmdir /s /q debug
 mingw32-make clean
+
+for /f "tokens=2 delims=\=" %%a in ('type src.pro^|find "VERSION = "') do (
+  set KLOGDEVELVERSION=%%a & goto :continue
+)
+:continue
+set KLOGDEVELVERSION=%KLOGDEVELVERSION:~1%
+echo Building KLog-%KLOGDEVELVERSION%
 
 qmake src.pro
 mingw32-make
 mkdir release
-xcopy /Y /S /F build\target release
+xcopy /Y /S /F build\target\* release
 echo localdir=%cd%
 echo %localdir%
 copy ..\..\paquete\openssl\*.dll release
