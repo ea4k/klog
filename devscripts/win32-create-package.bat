4b5bef8c97c3c8880d56bbf3a439d6345599820c
ea4k <jaime@robles.es>
2023-10-28 13:03:26 +0200

    New building files

--- devscripts/win32-create-package.bat
+++ devscripts/win32-create-package.bat
@@ -28,36 +28,30 @@ rem *                                                                           
 rem *****************************************************************************/@echo off
 echo Setting up environment for Qt usage...
 set KLOGDEVELVERSION=1
-set PATH=%PATH%;C:\Qt\5.15.2\mingw81_64\bin;C:\Qt\Tools\mingw810_64\bin;
-set PATH=%PATH%;C:\Program Files (x86)\BitRock InstallBuilder Enterprise 15.10.1\bin;
-echo Linea 10
+set PATH=%PATH%;C:/Qt/5.15.2/mingw81_32/bin;C:/Qt/Tools/mingw810_32/bin;
+set PATH=%PATH%;C:\Program Files\InstallBuilder Enterprise 23.10.1\bin;
+
 cd ../src/
 rmdir /S /Q build
 rmdir /S /Q release
 rmdir /S /Q debug
-echo Linea 20
 mingw32-make clean
-echo Linea 21
+
 for /f "tokens=2 delims=\=" %%a in ('type src.pro^|find "PKGVERSION = "') do (
   set KLOGDEVELVERSION=%%a & goto :continue
 )
-echo Linea 30
 :continue
 set KLOGDEVELVERSION=%KLOGDEVELVERSION:~1%
 echo Building KLog-%KLOGDEVELVERSION%
-echo Linea 40
-qmake -set CONFIG+=x86_64
+
 qmake src.pro
-echo Linea 41
 mingw32-make
-echo Linea 42
 mkdir release
-echo Linea 43
 xcopy /Y /S /F build\target\* release
 echo localdir=%cd%
 echo %localdir%
-copy ..\..\paquete\openssl\openssl-3\x64\bin\*.dll release
-copy ..\..\libs\hamlib-w64\bin\*.dll release
+copy ..\..\paquete\openssl\*.dll release
+copy ..\..\paquete\hamlibDLL\*.dll release
 windeployqt --qmldir qml release\klog.exe
 :: The SSL DLLs must be included and must match the version that were used to build Qt.
 :: Check in main.cpp and uncomment the SSL line to see what is the version that was used.
@@ -67,7 +61,7 @@ rem cd ..\..
 echo %cd%
 cd ../devscripts
 echo %cd%
-builder-cli.exe build klog-win64-build.xml --verbose
+builder-cli.exe build build-win32.xml --verbose
 
 
 
