b4799dbdd3cebb7653821dd6390272580df1b3fd
ea4k <jaime@robles.es>
2026-01-24 13:26:41 +0100

    licensing

--- src/dxcluster/bandrecommender.cpp
+++ src/dxcluster/bandrecommender.cpp
@@ -1,3 +1,29 @@
+/***************************************************************************
+    bandrecommender.cpp  -  description
+       -------------------
+begin                : jan 2026
+copyright            : (C) 2026 by Jaime Robles
+email                : jaime@robles.es
+***************************************************************************/
+
+/****************************************************************************
+* This file is part of KLog.                                                *
+*                                                                           *
+*    KLog is free software: you can redistribute it and/or modify           *
+*    it under the terms of the GNU General Public License as published by   *
+*    the Free Software Foundation, either version 3 of the License, or      *
+*    (at your option) any later version.                                    *
+*                                                                           *
+*    KLog is distributed in the hope that it will be useful,                *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+*    GNU General Public License for more details.                           *
+*                                                                           *
+*    You should have received a copy of the GNU General Public License      *
+*    along with KLog.  If not, see <https://www.gnu.org/licenses/>.         *
+*                                                                           *
+*****************************************************************************/
+
 #include "bandrecommender.h"
 
 BandRecommender::BandRecommender(QWidget *parent)
@@ -26,4 +52,29 @@ void BandRecommender::createUI()
     layout->addWidget(button4);
     layout->addWidget(button5);
     setLayout(layout);
+}
+
+void BandRecommender::setButton1(const QString _band)
+{
+    button1->setText(_band);
+}
+
+void BandRecommender::setButton2(const QString _band)
+{
+    button2->setText(_band);
+}
+
+void BandRecommender::setButton3(const QString _band)
+{
+    button3->setText(_band);
+}
+
+void BandRecommender::setButton4(const QString _band)
+{
+    button4->setText(_band);
+}
+
+void BandRecommender::setButton5(const QString _band)
+{
+    button5->setText(_band);
 }
--- src/dxcluster/bandrecommender.h
+++ src/dxcluster/bandrecommender.h
@@ -1,6 +1,30 @@
 #ifndef BANDRECOMMENDER_H
 #define BANDRECOMMENDER_H
+/***************************************************************************
+    bandrecommender.h  -  description
+       -------------------
+begin                : jan 2026
+copyright            : (C) 2026 by Jaime Robles
+email                : jaime@robles.es
+***************************************************************************/
 
+/****************************************************************************
+* This file is part of KLog.                                                *
+*                                                                           *
+*    KLog is free software: you can redistribute it and/or modify           *
+*    it under the terms of the GNU General Public License as published by   *
+*    the Free Software Foundation, either version 3 of the License, or      *
+*    (at your option) any later version.                                    *
+*                                                                           *
+*    KLog is distributed in the hope that it will be useful,                *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+*    GNU General Public License for more details.                           *
+*                                                                           *
+*    You should have received a copy of the GNU General Public License      *
+*    along with KLog.  If not, see <https://www.gnu.org/licenses/>.         *
+*                                                                           *
+*****************************************************************************/
 #include <QGroupBox>
 #include <QPushButton>
 #include <QVBoxLayout>
@@ -12,6 +36,11 @@ class BandRecommender : public QWidget
 public:
     explicit BandRecommender(QWidget *parent = nullptr);
     void createUI();
+    void setButton1(const QString _band);
+    void setButton2(const QString _band);
+    void setButton3(const QString _band);
+    void setButton4(const QString _band);
+    void setButton5(const QString _band);
 
 signals:
 
