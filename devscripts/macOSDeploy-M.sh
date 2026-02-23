3c1f7c1a14c12d217f678fa00172093c0be40a9b
ea4k <jaime@robles.es>
2026-02-23 13:41:33 +0100

    Update macOSDeploy-M.sh

--- devscripts/macOSDeploy-M.sh
+++ devscripts/macOSDeploy-M.sh
@@ -27,27 +27,71 @@
 # *                                                                           *
 # *****************************************************************************/
 export CXXFLAGS=-std=c++11
-#KLOG_VERSION="$1"
-KLOG_VERSION=$(grep "PKGVERSION =" src.pro |awk '{print $3}')
+KLOG_VERSION=$(grep "PKGVERSION =" src.pro | awk '{print $3}')
 echo "Packaging KLog-$KLOG_VERSION"
 KLOG_SOURCES="../src"
 QTDIRi=$HOME"/Qt/6.10.2/macos"
+
+# Cleaning
 rm -Rf KLog.app
 rm -Rf build 
 /usr/bin/make clean
+
+# Compiling
 "$QTDIRi"/bin/qmake6 src.pro -spec macx-clang 
 /usr/bin/make
+
+# Renaming for distribution
 mv build/target/klog.app KLog.app
-mkdir -p KLog.app/Contents/PlugIns/sqldrivers
-cp "$QTDIRi"/plugins/sqldrivers/libqsqlite.dylib KLog.app/Contents/PlugIns/sqldrivers
+
+# Adding the SQlite plugin
+#mkdir -p KLog.app/Contents/PlugIns/sqldrivers
+#cp "$QTDIRi"/plugins/sqldrivers/libqsqlite.dylib KLog.app/Contents/PlugIns/sqldrivers
+
+# Updating & Adding translations
 mkdir -p KLog.app/Contents/MacOS/translations
 $QTDIRi/bin/lupdate src.pro
 $QTDIRi/bin/lrelease src.pro
 cp "$KLOG_SOURCES"/translations/*.qm KLog.app/Contents/MacOS/translations/
-cp /opt/homebrew/Cellar/hamlib/4.6.5/lib//libhamlib.4.dylib KLog.app/Contents/MacOS/
+
+# Managing 3 party libraties
+
+## 1. Copy & Prepare libusb
+cp /opt/homebrew/opt/libusb/lib/libusb-1.0.0.dylib KLog.app/Contents/MacOS/
+chmod +w KLog.app/Contents/MacOS/libusb-1.0.0.dylib
+install_name_tool -id @executable_path/libusb-1.0.0.dylib KLog.app/Contents/MacOS/libusb-1.0.0.dylib
+
+# 2. Copy & prepare libhamlib
+cp /opt/homebrew/opt/hamlib/lib/libhamlib.4.dylib KLog.app/Contents/MacOS/
 chmod +w KLog.app/Contents/MacOS/libhamlib.4.dylib
 install_name_tool -id @executable_path/libhamlib.4.dylib KLog.app/Contents/MacOS/libhamlib.4.dylib
-install_name_tool -change /opt/homebrew/Cellar/hamlib/4.6.5/lib/libhamlib.4.dylib @executable_path/libhamlib.4.dylib KLog.app/Contents/MacOS/klog
+
+# 3. Fix the internal link, inform hamlib where is libusb
+install_name_tool -change /opt/homebrew/opt/libusb/lib/libusb-1.0.0.dylib @executable_path/libusb-1.0.0.dylib KLog.app/Contents/MacOS/libhamlib.4.dylib
+
+# 4. Fix the main binary (KLog) to find Hamlib
+install_name_tool -change /opt/homebrew/opt/hamlib/lib/libhamlib.4.dylib @executable_path/libhamlib.4.dylib KLog.app/Contents/MacOS/KLog
+
+# Fixing signatures
+#echo "Fixing broken signatures (Ad-Hoc)..."
+#codesign --force --deep --sign - KLog.app
+
+# Creating DMG. We create it with macdeployqt6 to ensure all the needed files are in the folder
+echo "Creating DMG..."
 "$QTDIRi"/bin/macdeployqt6 KLog.app/ -qmldir=./qml/ -dmg
+
+# Removing not needed plugins; macdeployqt6 includes more than needed
+rm -f KLog.app/Contents/PlugIns/sqldrivers/libqsqlmimer.dylib
+rm -f KLog.app/Contents/PlugIns/sqldrivers/libqsqlodbc.dylib
+rm -f KLog.app/Contents/PlugIns/sqldrivers/libqsqlpsql.dylib
+rm -f KLog.app/Contents/PlugIns/quick/libqtquickcontrols2iosstyleplugin.dylib
+# Remove the DMG file as we will build it with hdiutil
+rm -f KLog.dmg
+
+# Signing the full folder to prevent Kill 9 error
+codesign --force --deep --sign - KLog.app
+
+# Creating the final DMG file
+hdiutil create -volname "KLog" -srcfolder KLog.app -ov -format UDZO KLog.dmg
 mv KLog.dmg KLog-"$KLOG_VERSION".dmg
 echo "You can find the dmg file in this folder... enjoy KLog!"
