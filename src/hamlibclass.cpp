f54117e9e635bdf22be7096076fd5bf7e2b8f43a
ea4k <jaime@robles.es>
2026-05-01 17:02:13 +0200

    Update hamlibclass.cpp

--- src/hamlibclass.cpp
+++ src/hamlibclass.cpp
@@ -30,6 +30,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "utilities.h"
+#include <QElapsedTimer>
 
 HamLibClass::HamLibClass(QObject *parent) : QObject(parent)
 {
@@ -359,25 +360,33 @@ void HamLibClass::startPolling()
 bool HamLibClass::readRadioInternal()
 {
     logEvent(Q_FUNC_INFO, "Start", Devel);
+    QElapsedTimer timer;
+    timer.start();
+    qDebug() << Q_FUNC_INFO;
     if (!my_rig || (rig_state != RigState::Connected) )
         return false;
-
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 010" << timer.elapsed() << "ms"; timer.restart();
     RadioStatus statusOld = radioStatus;
     if(!readVFO())   return false;
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 020" << timer.elapsed() << "ms"; timer.restart();
     if (!readSplit()) return false;
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 030" << timer.elapsed() << "ms"; timer.restart();
     if(!readFreq())   return false;
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 040" << timer.elapsed() << "ms"; timer.restart();
     if (!readMode())  return false;
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 050" << timer.elapsed() << "ms"; timer.restart();
 
     errorCount = 0;
     if (radioStatusChanged(statusOld, radioStatus))
         emit radioStatusChanged(radioStatus);
-
+    qInfo() << Q_FUNC_INFO << " [KLOG-TIMING] 060" << timer.elapsed() << "ms"; timer.restart();
     //reading = false;
     return true;
 }
 
 bool HamLibClass::radioStatusChanged(const RadioStatus _old, const RadioStatus _new)
 {
+    qDebug() << Q_FUNC_INFO << " - 000";
     return (
         _old.split != _new.split                ||
         _old.memoryMode != _new.memoryMode      ||
@@ -391,14 +400,14 @@ bool HamLibClass::radioStatusChanged(const RadioStatus _old, const RadioStatus _
 void HamLibClass::slotTimer()
 {
     logEvent(Q_FUNC_INFO, "Start", Devel);
-    //qDebug() << Q_FUNC_INFO;
+    qDebug() << Q_FUNC_INFO;
     if (!isRunning())
     {
-        //qDebug() << Q_FUNC_INFO << ": Isn't running...";
+        qDebug() << Q_FUNC_INFO << ": Isn't running...";
         return;
     }
     readRadioInternal(); // We don't force the radio reading.
-    //qDebug() << Q_FUNC_INFO << " - END";
+    qDebug() << Q_FUNC_INFO << " - END";
 }
 
 void HamLibClass::setMode(const QString &_m)
